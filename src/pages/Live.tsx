c1589a24cbb6097af43a9d1b42a1cde27f7b7fcc
SmartSoil-SPCBA <github.spcba@gmail.com>
2025-12-12 10:47:56 +0800

    Reccomendation text issue fix

--- src/pages/Live.tsx
+++ src/pages/Live.tsx
@@ -269,11 +269,11 @@ export default function Live() {
       // Check if the parameter is out of range
       if (numVal < min) {
         issues.push(
-          `• ${p.label} is LOW (${numVal}${unit ? ' ' + unit : ''}; optimal ${min}–${max} ${unit}). ${baseDesc}`
+          `<br>• ${p.label} is LOW (${numVal}${unit ? ' ' + unit : ''}; optimal ${min}–${max} ${unit}). ${baseDesc}</br>`
         );
       } else if (numVal > max) {
         issues.push(
-          `• ${p.label} is HIGH (${numVal}${unit ? ' ' + unit : ''}; optimal ${min}–${max} ${unit}). ${baseDesc}`
+          `<br>• ${p.label} is HIGH (${numVal}${unit ? ' ' + unit : ''}; optimal ${min}–${max} ${unit}). ${baseDesc}</br>`
         );
       }
     });
